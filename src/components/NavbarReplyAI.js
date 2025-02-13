38ce35afa15bf1cd27df233fee1a7cca67b9e4bb
davidmrtns <daviddmrtns@gmail.com>
2025-02-12 22:50:28 -0300

    Adição da logo do ReplyAI

--- src/components/NavbarReplyAI.js
+++ src/components/NavbarReplyAI.js
@@ -7,11 +7,12 @@ import { faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import ApiFetch from '../utils/ApiFetch';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { ReactComponent as ReplyAILogo } from '../replyai-logo.svg';
 
 function NavbarReplyAI(){
     const apiFetch = new ApiFetch();
     const navigate = useNavigate();
-    const { usuarioLogado } = useAuth();
+    const { usuarioLogado } = useAuth() || {};
 
     const deslogar = async () => {
         var resposta = await apiFetch.logout();
@@ -23,7 +24,7 @@ function NavbarReplyAI(){
     return(
         <Navbar expand="lg" className="navbar-replyai fixed-top" variant="dark">
             <Container fluid>
-                <Navbar.Brand href="/" className="titulo">ReplyAI</Navbar.Brand>
+                <Navbar.Brand href="/" className="titulo"><ReplyAILogo className="logo" /> ReplyAI</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}>
